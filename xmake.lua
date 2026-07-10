d332f04ef232e12e2218cd341f1ecc7fdcedd145
wed150 <wed15058338634@outlook.com>
2026-07-10 21:12:00 +0800

    feat: adapt to Levilamina 26.20.0

--- xmake.lua
+++ xmake.lua
@@ -2,10 +2,10 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_repositories("groupmountain-repo https://github.com/GroupMountain/xmake-repo.git")
-add_requires("levilamina 26.10.5", {configs = {target_type = "server"}})
+add_requires("levilamina 26.20.0", {configs = {target_type = "server"}})
 add_requires("levibuildscript")
 
-add_requires("gmlib 26.10.0")
+add_requires("gmlib 26.20.0")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
@@ -15,7 +15,25 @@ target("InventoryCheck")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
     add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
-    add_defines("NOMINMAX", "UNICODE")
+    add_cxflags(
+        "/EHs",
+        "-Wno-microsoft-cast",
+        "-Wno-invalid-offsetof",
+        "-Wno-c++2b-extensions",
+        "-Wno-microsoft-include",
+        "-Wno-overloaded-virtual",
+        "-Wno-ignored-qualifiers",
+        "-Wno-missing-field-initializers",
+        "-Wno-potentially-evaluated-expression",
+        "-Wno-pragma-system-header-outside-header",
+        {tools = {"clang_cl"}}
+    )
+    set_toolchains("clang-cl")
+    add_defines(
+        "NOMINMAX",
+        "UNICODE",
+        "_HAS_CXX23=1"
+    )
     add_packages("levilamina","gmlib")
     set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
